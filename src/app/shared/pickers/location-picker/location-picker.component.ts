473bf92ea5a24446df1c48834b20a68fc55c460b
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-06-06 11:56:32 -0300

    feat(map): shapes picker and modal, add shared mod

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { MapModalComponent } from '../../map-modal/map-modal.component';
 
 @Component({
   selector: 'app-location-picker',
   templateUrl: './location-picker.component.html',
   styleUrls: ['./location-picker.component.scss'],
 })
-export class LocationPickerComponent  implements OnInit {
-
-  constructor() { }
+export class LocationPickerComponent implements OnInit {
+  constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
-
+  onPickLocation() {
+    this.modalController
+      .create({ component: MapModalComponent })
+      .then((el) => el.present());
+  }
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapModalComponent } from './map-modal/map-modal.component';
+import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [LocationPickerComponent, MapModalComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [LocationPickerComponent, MapModalComponent],
+})
+export class SharedModule {}

1bf9183c7ca4e0c219e50e3bea6b5ec78802cb30
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-06-09 10:26:45 -0300

    build: add capacitor/android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ng.pairbnb.course',
   appName: 'ionic-angular-course2',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
